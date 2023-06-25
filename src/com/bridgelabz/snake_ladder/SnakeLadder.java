7f47609f2574e1b5d428c9493dc1890b55e2b23f
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-06-26 00:29:27 +0530

    The Player rolls the die to get a number between 1 to 6

--- src/com/bridgelabz/snake_ladder/SnakeLadder.java
+++ src/com/bridgelabz/snake_ladder/SnakeLadder.java
@@ -2,7 +2,9 @@ package com.bridgelabz.snake_ladder;
 
 public class SnakeLadder {
     public static void main(String[] args) {
-        int playerPosition1 = 0;
-        System.out.println("Player1 at starting position is: " + playerPosition1);
+        int player1Position = 0;
+        System.out.println("Player1 at starting position is: " + player1Position);
+        int dieValue = (int) (Math.random() * 10 % 6) + 1;
+        System.out.println("Die value : " + dieValue);
     }
 }
