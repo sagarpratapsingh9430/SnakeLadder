02d5e144356249a1401ab8f2adba4127acd828af
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-06-26 00:27:13 +0530

    Snake and Ladder game played with single player at start position 0

---
+++ src/com/bridgelabz/snake_ladder/SnakeLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.snake_ladder;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        int playerPosition1 = 0;
+        System.out.println("Player1 at starting position is: " + playerPosition1);
+    }
+}
