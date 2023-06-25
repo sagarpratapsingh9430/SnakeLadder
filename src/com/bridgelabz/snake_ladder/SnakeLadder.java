64a45b1a2a5e5ea3cc39db42de4906fac941fd71
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-06-26 00:31:09 +0530

    The Player then checks for a Option. They are No Play, Ladder or Snake.

--- src/com/bridgelabz/snake_ladder/SnakeLadder.java
+++ src/com/bridgelabz/snake_ladder/SnakeLadder.java
@@ -1,10 +1,35 @@
 package com.bridgelabz.snake_ladder;
 
 public class SnakeLadder {
+    static final int WIN_POSITION = 100;
+
+    static final int START_POSITION = 0;
+    static final int NO_PLAY = 1;
+    static final int LADDER = 2;
+    static final int SNAKE = 3;
+
+    static int player1Position = 0;
     public static void main(String[] args) {
-        int player1Position = 0;
-        System.out.println("Player1 at starting position is: " + player1Position);
+
         int dieValue = (int) (Math.random() * 10 % 6) + 1;
-        System.out.println("Die value : " + dieValue);
+        int option = (int) (Math.random() * 10 % 3) + 1;
+
+        switch (option) {
+            case NO_PLAY:
+                System.out.println("No Play");
+                break;
+            case LADDER:
+                if (player1Position + dieValue <= WIN_POSITION)
+                    player1Position = player1Position + dieValue;
+                break;
+            case SNAKE:
+                player1Position = player1Position - dieValue;
+                if (player1Position < START_POSITION)
+                    player1Position = START_POSITION;
+                break;
+            default:
+                System.out.println("wrong option");
+
+        }
     }
 }
